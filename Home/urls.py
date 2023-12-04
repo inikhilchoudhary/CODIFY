b37ea0fa12016a85a8c1d7bae2f5dad6a723bc69
Nikhil Choudhary <chnikhilmanadarng@gmail.com>
2023-12-04 17:54:05 +0530

    commit date4Dec

--- Home/urls.py
+++ Home/urls.py
@@ -5,10 +5,13 @@ from django.urls import path
 from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/admin/', admin.site.urls),
     path("", views.index, name='home'),  # Assign the name 'home' to your home view
     path("Subjects", views.subjects, name='subjects'),
     path("SourceCode", views.source_code, name='source_code'),
     path("Contribute", views.contribute, name='contribute'),
     path("AboutUs", views.about_us, name='about_us'),
+    path("Contact", views.contact, name='contact'),
+    path("TermsAndCondition", views.tandc, name='tandc'),
+    path("PrivacyAndPolicy", views.privacyandpolicy, name='privacyandpolicy'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from Home.models import Contact
 
 def index(request):
     #return HttpResponse("This is homepage")
@@ -22,4 +23,23 @@ def contribute(request):
 def about_us(request):
     #return HttpResponse("This is About Us page")
     return render(request,'about.html')
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+        #datetime.today()
+        contact=Contact(name=name,email=email,message=message,date=datetime.today())
+        
+    #return HttpResponse("This is Contact Us page")
+    return render(request,'contact.html')
+
+def tandc(request):
+    #return HttpResponse("This is Terms and Condition  page")
+    return render(request,'tandc.html')
+
+def privacyandpolicy(request):
+    #return HttpResponse("This is Privacy and Policy page")
+    return render(request,'privacyandpolicy.html')
 
